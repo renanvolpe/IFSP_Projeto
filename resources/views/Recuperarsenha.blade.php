88226b6e3181e15cd79952f3c309545cb188941b
Salgreis <55090998+Salgreis@users.noreply.github.com>
2020-12-18 00:47:54 -0300

    Recuperar Senha
    Mudança na colocação da tela de recuperar senha

---
+++ resources/views/Recuperarsenha.blade.php
@@ -0,0 +1,55 @@
+<!doctype html>
+<html>
+	<head>
+		
+		<meta name="viewport" content="width=device-width,initial-scale=1,shrink-to-fit=no" />
+
+		<link rel="stylesheet" href="bootstrap/bootstrap.min.css" />
+		<link rel="stylesheet" href="resources/css/style.css">
+
+		<title>Recuperar senha</title>
+
+		</head>
+
+		<body>
+			<div class="container">
+				<div class="row">
+					<div class="">
+					
+					<div class="col-6">
+				<div class="media">
+				
+			<form action="/action_page.php">
+				<div class="">
+				<h1>RECUPERAR SENHA</h1>
+				</div>
+				<div class="">
+				<div class="form-group">
+				  <label id="texto" for="email">CPF</label>
+				  <input type="email" class="form-control" placeholder="Digite o CPF" id="email">
+				</div>
+				<div class="form-group">
+				  <label id="texto" for="pwd">E-mail</label>
+				  <input type="password" class="form-control" placeholder="Digite o e-mail" id="pwd">
+				</div>
+				<div class="form-group form-check">
+				  <label id="texto" class="form-check-label">
+					<input class="form-check-input" type="checkbox"> Lembrar-me
+				  </label>
+				</div>
+			</div>
+				<button type="submit" class="btn btn-primary">Enviar</button>
+			  </form>
+			</div>
+			</div>
+			  <div class="col-6">
+			  <img class="img-fluid" src="logomedicina.png" alt="Generic placeholder image">
+			</div>
+			</div>
+			</div>
+			</div>
+
+		<script type="text/javascript" src="bootstrap/jquery-3.5.1.min.js"></script>
+		<script type="text/javascript" src="bootstrap/bootstrap.bundle.js"></script>
+		</body>
+		</html>
