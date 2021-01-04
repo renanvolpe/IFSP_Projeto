9eae1d3393d801dd5dc405cc1e091081f8fd002d
Aline Oliveira <44536353+Aline-o@users.noreply.github.com>
2021-01-03 23:14:39 -0300

    bd ajuste
    ajuste no banco, precisa de revisão

--- clinicamedica.sql
+++ clinicamedica.sql
@@ -3,9 +3,9 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: 127.0.0.1
--- Tempo de geração: 07-Dez-2020 às 04:41
--- Versão do servidor: 10.4.14-MariaDB
--- versão do PHP: 7.4.10
+-- Tempo de geração: 04-Jan-2021 às 03:11
+-- Versão do servidor: 10.4.11-MariaDB
+-- versão do PHP: 7.4.6
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 START TRANSACTION;
@@ -24,102 +24,81 @@ SET time_zone = "+00:00";
 -- --------------------------------------------------------
 
 --
+-- Estrutura da tabela `agenda`
+--
+
+CREATE TABLE `agenda` (
+  `idAgenda` int(11) NOT NULL,
+  `horario` time NOT NULL,
+  `dia` date NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Estrutura da tabela `consulta`
+--
+
+CREATE TABLE `consulta` (
+  `idConsulta` int(11) NOT NULL,
+  `realizada` tinyint(1) NOT NULL,
+  `sintoma` varchar(245) NOT NULL,
+  `tratamento` varchar(245) DEFAULT NULL,
+  `observacao` varchar(255) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
 -- Estrutura da tabela `endereco`
 --
 
 CREATE TABLE `endereco` (
-  `rua` varchar(20) NOT NULL,
+  `idEndereco` int(11) NOT NULL,
+  `rua` varchar(40) NOT NULL,
   `bairro` varchar(20) NOT NULL,
-  `numero` int(11) NOT NULL,
+  `numero` varchar(5) NOT NULL,
   `estado` varchar(20) NOT NULL,
   `cidade` varchar(20) NOT NULL,
-  `cep` varchar(20) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+  `cep` varchar(8) NOT NULL,
+  `complemento` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Extraindo dados da tabela `endereco`
+--
+
+INSERT INTO `endereco` (`idEndereco`, `rua`, `bairro`, `numero`, `estado`, `cidade`, `cep`, `complemento`) VALUES
+(1, 'oioi', 'uii', '89', 'sp', 'jcr', '1212', 'oiy');
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `failed_jobs`
+-- Estrutura da tabela `login`
 --
 
-CREATE TABLE `failed_jobs` (
-  `id` bigint(20) UNSIGNED NOT NULL,
-  `uuid` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `connection` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `queue` text COLLATE utf8mb4_unicode_ci NOT NULL,
-  `payload` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `exception` longtext COLLATE utf8mb4_unicode_ci NOT NULL,
-  `failed_at` timestamp NOT NULL DEFAULT current_timestamp()
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+CREATE TABLE `login` (
+  `idLogin` varchar(20) NOT NULL,
+  `login` varchar(20) NOT NULL,
+  `senha` varchar(20) NOT NULL,
+  `priviegios` varchar(20) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
 --
--- Estrutura da tabela `medicamento`
+-- Estrutura da tabela `paciente`
 --
 
-CREATE TABLE `medicamento` (
-  `codMedicamento` int(11) NOT NULL,
-  `laboratorio` varchar(245) DEFAULT NULL,
-  `nomeMedicamento` varchar(245) DEFAULT NULL,
-  `dosagem` float DEFAULT NULL,
-  `receita` varchar(255) DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `migrations`
---
-
-CREATE TABLE `migrations` (
-  `id` int(10) UNSIGNED NOT NULL,
-  `migration` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `batch` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Extraindo dados da tabela `migrations`
---
-
-INSERT INTO `migrations` (`id`, `migration`, `batch`) VALUES
-(1, '2014_10_12_000000_create_users_table', 1),
-(2, '2014_10_12_100000_create_password_resets_table', 1),
-(3, '2019_08_19_000000_create_failed_jobs_table', 1);
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `particular`
---
-
-CREATE TABLE `particular` (
-  `statusPagto` varchar(25) NOT NULL,
-  `valor` float NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `password_resets`
---
-
-CREATE TABLE `password_resets` (
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `token` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `created_at` timestamp NULL DEFAULT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `statusatendimento`
---
-
-CREATE TABLE `statusatendimento` (
-  `idStatusAt` int(11) NOT NULL,
-  `descStatusAt` varchar(30) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
+CREATE TABLE `paciente` (
+  `idCpf` varchar(11) NOT NULL,
+  `nome` varchar(45) NOT NULL,
+  `dataNascimento` date NOT NULL,
+  `email` varchar(20) NOT NULL,
+  `tipoSanguineo` varchar(3) DEFAULT NULL,
+  `sexo` char(2) NOT NULL,
+  `foto` varchar(70) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 -- --------------------------------------------------------
 
@@ -129,106 +108,45 @@ CREATE TABLE `statusatendimento` (
 
 CREATE TABLE `telefone` (
   `idTelefone` int(20) NOT NULL,
-  `ddd` int(20) NOT NULL,
-  `telefonePrincipal` varchar(20) NOT NULL,
-  `telefoneOpcional` varchar(20) DEFAULT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `tipousuario`
---
-
-CREATE TABLE `tipousuario` (
-  `idTipoUsuario` int(3) NOT NULL,
-  `descTipoUsuario` varchar(120) NOT NULL
-) ENGINE=MyISAM DEFAULT CHARSET=latin1;
-
--- --------------------------------------------------------
-
---
--- Estrutura da tabela `users`
---
-
-CREATE TABLE `users` (
-  `id` int(20) UNSIGNED NOT NULL,
-  `name` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `email_verified_at` timestamp NULL DEFAULT NULL,
-  `password` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `remember_token` varchar(100) COLLATE utf8mb4_unicode_ci DEFAULT NULL,
-  `created_at` timestamp NULL DEFAULT NULL,
-  `updated_at` timestamp NULL DEFAULT NULL,
-  `cpf` varchar(14) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `dd1` int(5) NOT NULL,
-  `dd2` int(5) NOT NULL,
-  `telefone` int(20) NOT NULL,
-  `telefonesecundario` int(20) NOT NULL,
-  `tiposanguineo` int(3) NULL,
-  `endereco` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `numero` int(20) NOT NULL,
-  `estado` varchar(2) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `bairro` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `cep` int(15) NOT NULL,
-  `cidade` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `confirmaremail` varchar(255) COLLATE utf8mb4_unicode_ci NOT NULL,
-  `confirmarpassword` varchar(3) COLLATE utf8mb4_unicode_ci NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
-
---
--- Extraindo dados da tabela `users`
---
-
-INSERT INTO `users` (`id`, `name`, `email`, `email_verified_at`, `password`, `remember_token`, `created_at`, `updated_at`, `cpf`, `dd1`, `dd2`, `telefone`, `telefonesecundario`, `tiposanguineo`, `endereco`, `numero`, `estado`, `bairro`, `cep`, `cidade`, `confirmaremail`, `confirmarpassword`) VALUES
-(5, 'alex', 'alexfs@gmail.com', NULL, '$2y$10$5LsMZmhRbsd66OfWTaZVc.0OqacJhQ4/EsoZmR/Z6viDrqKxiuRMy', NULL, '2020-12-01 05:41:23', '2020-12-01 05:41:23', '', 0, 0, 0, 0, 0, '', 0, '', '', 0, '', '', '');
+  `telPrimario` varchar(11) NOT NULL,
+  `telSecundario` varchar(11) DEFAULT NULL,
+  `celular1` varchar(11) NOT NULL,
+  `celular2` varchar(11) DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
 -- Índices para tabelas despejadas
 --
 
 --
+-- Índices para tabela `agenda`
+--
+ALTER TABLE `agenda`
+  ADD PRIMARY KEY (`idAgenda`);
+
+--
+-- Índices para tabela `consulta`
+--
+ALTER TABLE `consulta`
+  ADD PRIMARY KEY (`idConsulta`);
+
+--
 -- Índices para tabela `endereco`
 --
 ALTER TABLE `endereco`
-  ADD PRIMARY KEY (`cep`);
+  ADD PRIMARY KEY (`idEndereco`);
 
 --
--- Índices para tabela `failed_jobs`
+-- Índices para tabela `login`
 --
-ALTER TABLE `failed_jobs`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `failed_jobs_uuid_unique` (`uuid`);
+ALTER TABLE `login`
+  ADD PRIMARY KEY (`idLogin`);
 
 --
--- Índices para tabela `medicamento`
+-- Índices para tabela `paciente`
 --
-ALTER TABLE `medicamento`
-  ADD PRIMARY KEY (`codMedicamento`);
-
---
--- Índices para tabela `migrations`
---
-ALTER TABLE `migrations`
-  ADD PRIMARY KEY (`id`);
-
---
--- Índices para tabela `particular`
---
-ALTER TABLE `particular`
-  ADD PRIMARY KEY (`statusPagto`);
-
---
--- Índices para tabela `password_resets`
---
-ALTER TABLE `password_resets`
-  ADD KEY `password_resets_email_index` (`email`);
-
---
--- Índices para tabela `statusatendimento`
---
-ALTER TABLE `statusatendimento`
-  ADD PRIMARY KEY (`descStatusAt`);
+ALTER TABLE `paciente`
+  ADD PRIMARY KEY (`idCpf`);
 
 --
 -- Índices para tabela `telefone`
@@ -237,43 +155,20 @@ ALTER TABLE `telefone`
   ADD PRIMARY KEY (`idTelefone`);
 
 --
--- Índices para tabela `tipousuario`
---
-ALTER TABLE `tipousuario`
-  ADD PRIMARY KEY (`descTipoUsuario`);
-
---
--- Índices para tabela `users`
---
-ALTER TABLE `users`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `users_email_unique` (`email`),
-  ADD UNIQUE KEY `cpf_2` (`cpf`),
-  ADD UNIQUE KEY `confirmaremail` (`confirmaremail`),
-  ADD UNIQUE KEY `confirmarpassword` (`confirmarpassword`);
-ALTER TABLE `users` ADD FULLTEXT KEY `cpf` (`cpf`);
-
---
 -- AUTO_INCREMENT de tabelas despejadas
 --
 
 --
--- AUTO_INCREMENT de tabela `failed_jobs`
+-- AUTO_INCREMENT de tabela `agenda`
 --
-ALTER TABLE `failed_jobs`
-  MODIFY `id` bigint(20) UNSIGNED NOT NULL AUTO_INCREMENT;
+ALTER TABLE `agenda`
+  MODIFY `idAgenda` int(11) NOT NULL AUTO_INCREMENT;
 
 --
--- AUTO_INCREMENT de tabela `migrations`
+-- AUTO_INCREMENT de tabela `endereco`
 --
-ALTER TABLE `migrations`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
-
---
--- AUTO_INCREMENT de tabela `users`
---
-ALTER TABLE `users`
-  MODIFY `id` int(20) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=8;
+ALTER TABLE `endereco`
+  MODIFY `idEndereco` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
