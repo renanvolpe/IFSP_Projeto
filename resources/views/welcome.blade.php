1ea6e9025d90ef1aae44bf630d580c6385edbc99
Volpe <renan.volpe@aluno.ifsp.edu.br>
2021-01-07 00:49:36 -0300

    arrumei as imagens pra inserir por meio do laravel

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -735,7 +735,7 @@ The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for t
 
             <div class="row">
                 <div class="col-md-3">
-                    <img src="/assets/especialidade.png" style="width:80%">
+                    <img src="{{ URL::to('/assets/especialidade.png') }}" style="width:80%">
                 </div>
                 <div class="col-md-8">
                     <p> Contrary to popular belief, Lorem Ipsum is not simply random text. It has roots in a piece of classical Latin literature from 45 BC, making it over 2000 years old. Richard McClintock, a Latin professor at Hampden-Sydney College in Virginia, looked up one of the more obscure Latin words, consectetur, from a Lorem Ipsum passage, and going through the cites of the word in classical literature, discovered the undoubtable source. Lorem Ipsum comes from sections 1.10.32 and 1.10.33 of "de Finibus Bonorum et Malorum" (The Extremes of Good and Evil) by Cicero, written in 45 BC. This book is a treatise on the theory of ethics, very popular during the Renaissance. The first line of Lorem Ipsum, "Lorem ipsum dolor sit amet..", comes from a line in section 1.10.32.
@@ -753,7 +753,7 @@ The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for t
                     </p>
                 </div>
                 <div class="col-md-3">
-                    <img src="/assets/especialidade2.png" style="width:80%">
+                    <img src="{{ URL::to('/assets/especialidade2.png') }}" style="width:80%">
 
                 </div>
             </div>
@@ -762,32 +762,32 @@ The standard chunk of Lorem Ipsum used since the 1500s is reproduced below for t
                 <div class="row">
 
                     <div class="col-md-4">
-                        <img src="/assets/cancerprostota.png" style="width:50%">
+                        <img src="{{ URL::to('/assets/cancerprostota.png') }}" style="width:50%">
                         <h5>Tratamento 1</h5>
                     </div>
 
                     <div class="col-md-4">
-                        <img src="/assets/cancerbexiga.jpg" style="width:50%">
+                        <img src="{{ URL::to('/assets/cancerbexiga.jpg') }}" style="width:50%">
                         <h5>Tratamento 2</h5>
                     </div>
 
                     <div class="col-md-4">
-                        <img src="/assets/cancerrim.png" style="width:30%">
+                        <img src="{{ URL::to('/assets/cancerrim.png') }}" style="width:30%">
                         <h5>Tratamento 3</h5>
                     </div>
 
                     <div class="col-md-4">
-                        <img src="/assets/ejaculacaoprecoce.png" style="width:30%">
+                        <img src="{{ URL::to('/assets/ejaculacaoprecoce.png') }}" style="width:30%">
                         <h5>Tratamento 4</h5>
                     </div>
 
                     <div class="col-md-4">
-                        <img src="/assets/pedrarim.png" style="width:50%">
+                        <img src="{{ URL::to('/assets/pedrarim.png') }}" style="width:50%">
                         <h5>Tratamento 5</h5>
                     </div>
 
                     <div class="col-md-4">
-                        <img src="/assets/fimose.png" style="width:30%">
+                        <img src="{{ URL::to('/assets/fimose.png') }}" style="width:30%">
                         <h5>Tratamento 6</h5>
                     </div>
 
