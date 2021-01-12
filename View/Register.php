7b2244ddd255f66cabb89589dc0d5d6c9c10c490
Aline Oliveira <alineoliveira199926@hotmail.com>
2021-01-12 17:53:47 -0300

    inserção dos campos faltando
    inserção dos campos que estavam faltando, vulgo Sexo e Tipo sanguineo

--- View/Register.php
+++ View/Register.php
@@ -43,64 +43,86 @@ include '../ChamarBoostrap.php';
 					<div class="row form-group"  >
 							<div class="col-md-9 "  >
 							<div class="form-group">
-							<label for="exampleInputEmail1">Nome Completo</label>
-							<input type="email" class="form-control" id="exampleInputEmail1"  placeholder="Nome Completo" name="NomeCompleto">
+							<label for="NomeCompleto">Nome Completo:</label>
+							<input type="text" class="form-control" id="NomeCompleto"  placeholder="Nome Completo" name="NomeCompleto" autofocus>
 							
 							</div>
 							</div>
-							<div class="col-md-3 control-label" style="padding-bottom: 10px;">
+							<div class="col-md-3 control-label">
+								<div class="form-group">
+									<label for="tipoSangue">Tipo Sanguíneo:</label>
+									<select class="form-control" id="tipoSangue">
+										<option value="N/a" selected>Não sei</option>
+										<option value="A+">A+</option>
+										<option value="A-">A-</option>
+										<option value="B+">B+</option>
+										<option value="B-">B-</option>
+										<option value="O+">O+</option>
+										<option value="O-">O-</option>
+										<option value="AB+">AB+</option>
+										<option value="AB-">AB-</option>
+									</select>
+								</div>
+							</div>
+
+							<div class="col-md-2">
 								<div class="form-floating">
-								<label for="floatingInputGrid">Tipo Sanguíneo:</label>
-								<input type="text" class="form-control" id="floatingInputGrid" placeholder="Sangue" value="" name="TipoSangue">
+								<label for="ddd1">DDD:</label>
+								<input type="text" class="form-control" id="ddd1" placeholder="(__)" value="" name="ddd1">
 								
 								</div>
 							</div>
 
 							<div class="col-md-4">
 								<div class="form-floating">
-								<label for="floatingInputGrid">Telefone Principal:</label>
-								<input type="text" class="form-control" id="floatingInputGrid" placeholder="Telefone Principal" value="" name="telefonePrincipal">
+								<label for="telefonePrincipal">Telefone Principal:</label>
+								<input type="text" class="form-control" id="telefonePrincipal" placeholder="xxxx-xxxx" value="" name="telefonePrincipal">
 								
 								</div>
 							</div>
 
 							<div class="col-md-2">
 								<div class="form-floating">
-								<label for="floatingInputGrid">DDD:</label>
-								<input type="text" class="form-control" id="floatingInputGrid" placeholder="DDD" value="" name="ddd1">
+								<label for="ddd2">DDD:</label>
+								<input type="text" class="form-control" id="ddd2" placeholder="(__)" value="" name="ddd2">
 								
 								</div>
 							</div>
+							
+							<div class="col-md-4">
+								<div class="form-floating">
+								<label for="celular">Celular:</label>
+								<input type="text" class="form-control" id="celular" placeholder="xxxxx-xxxx" value="" name="celular">
+								
+								</div>
+							</div>
+
+							
 
 							<div class="col-md-4">
 								<div class="form-floating">
-								<label for="floatingInputGrid">Telefone Secundário:</label>
-								<input type="text" class="form-control" id="floatingInputGrid" placeholder="Telefone Principal" value="" name="telefoneSecond">
-								
-								</div>
-							</div>
-
-							<div class="col-md-2">
-								<div class="form-floating">
-								<label for="floatingInputGrid">DDD:</label>
-								<input type="text" class="form-control" id="floatingInputGrid" placeholder="DDD" value="" name="ddd2">
-								
-								</div>
-							</div>
-
-							<div class="col-md-4">
-								<div class="form-floating">
-								<label for="floatingInputGrid">CPF:</label>
-								<input type="text" class="form-control" id="floatingInputGrid" placeholder="CPF" value="" name="CPF_Paciente">
+								<label for="CPF_Paciente">CPF:</label>
+								<input type="text" class="form-control" id="CPF_Paciente" placeholder="CPF" value="" name="CPF_Paciente">
 								
 								</div>
 							</div>
 
 							<div class="col-md-4 ">
 								<div class="form-floating">
-								<label for="floatingInputGrid">Data nascimento:</label>
-								<input type="text" class="form-control" id="floatingInputGrid" placeholder="<?php echo "date"; ?>" value="" name="Nascimento">
+								<label for="Nascimento">Data nascimento:</label>
+								<input type="date" class="form-control" id="Nascimento" placeholder="<?php echo "date"; ?>" value="" name="Nascimento">
 								
+								</div>
+							</div>
+
+							<div class="col-md-4 control-label">
+								<div class="form-group">
+									<label for="sexo">Sexo:</label>
+									<select class="form-control" id="sexo">
+										<option value="X" selected>Prefiro não dizer</option>
+										<option value="F">F</option>
+										<option value="M">M</option>
+									</select>
 								</div>
 							</div>
 					</div>
@@ -112,77 +134,83 @@ include '../ChamarBoostrap.php';
 
 							<div class="col-md-8">
 								<div class="form-group">
-								<label for="exampleInputEmail1">Endereco:</label>
-								<input type="Text" class="form-control" id=""  placeholder="Endereco" name="Endereco">
+								<label for="Rua">Rua:</label>
+								<input type="Text" class="form-control" id="Rua"  placeholder="Rua" name="Rua">
 								</div>
 							</div>
 
 							<div class="col-md-2">
 							<div class="form-group">
-							<label for="exampleInputEmail1">Numero:</label>
-							<input type="Text" class="form-control" id=""  placeholder="Numero" name="Numero">
+							<label for="Numero">Número:</label>
+							<input type="Text" class="form-control" id="Numero"  placeholder="Número" name="Numero">
 								</div>
 							</div>
 
 							<div class="col-md-2">
 							<div class="form-group">
-							<label for="exampleInputEmail1">Estado:</label>
-							<input type="Text" class="form-control" id=""  placeholder="Estado" name="Estado">
+							<label for="Estado">Estado:</label>
+							<input type="Text" class="form-control" id="Estado"  placeholder="Estado" name="Estado">
 								</div>
 							</div>
 
 							<div class="col-md-4">
 							<div class="form-group">
-							<label for="exampleInputEmail1">Bairro:</label>
-							<input type="Text" class="form-control" id=""  placeholder="Numero" name="Numero">
+							<label for="Bairro">Bairro:</label>
+							<input type="Text" class="form-control" id="Bairro"  placeholder="Bairro" name="Bairro">
 								</div>
 							</div>
 
 							<div class="col-md-4">
 							<div class="form-group">
-							<label for="exampleInputEmail1">CEP:</label>
-							<input type="Text" class="form-control" id=""  placeholder="CEP" name="CEP">
+							<label for="CEP">CEP:</label>
+							<input type="Text" class="form-control" id="CEP"  placeholder="CEP" name="CEP">
 								</div>
 							</div>
 
 							<div class="col-md-4">
 							<div class="form-group">
-							<label for="exampleInputEmail1">Cidade:</label>
-							<input type="Text" class="form-control" id=""  placeholder="Cidade" name="Cidade">
+							<label for="Cidade">Cidade:</label>
+							<input type="Text" class="form-control" id="Cidade"  placeholder="Cidade" name="Cidade">
 								</div>
 							</div>
-
+							
+							<div class="col-md-12">
+							<div class="form-group">
+							<label for="Complemento">Complemento:</label>
+							<input type="Text" class="form-control" id="Complemento"  placeholder="Complemento" name="Complemento">
+								</div>
+							</div>
 							
 							</div>
 
 							<h3> Dados de Email: </h3>
-					<div class="row">
+					<div class="row form-group">
 							<div class="col-md-6">
 							<div class="form-group">
-							<label for="exampleInputEmail1">Email:</label>
-							<input type="email" class="form-control" id=""  placeholder="Email" name="Email">
+							<label for="Email">Email:</label>
+							<input type="email" class="form-control" id="Email"  placeholder="Email" name="Email">
 								</div>
 							</div>
 
 
 							<div class="col-md-6">
 							<div class="form-group">
-							<label for="exampleInputEmail1">Confirmar Email:</label>
-							<input type="email" class="form-control" id=""  placeholder="Confirmar Email" >
+							<label for="confEmail">Confirmar Email:</label>
+							<input type="email" class="form-control" id="confEmail" name="confEmail" placeholder="Confirmar Email" >
 								</div>
 							</div>
 
 							<div class="col-md-6">
 							<div class="form-group">
-							<label for="exampleInputEmail1">Senha:</label>
-							<input type="password" class="form-control" id=""  placeholder="Senha" name="Senha" >
+							<label for="Senha">Senha:</label>
+							<input type="password" class="form-control" id="Senha"  placeholder="Senha" name="Senha" >
 								</div>
 							</div>
 
 							<div class="col-md-6">
 							<div class="form-group">
-							<label for="exampleInputEmail1">Confirmar Senha:</label>
-							<input type="password" class="form-control" id=""  placeholder="Cofirmar Senha"  >
+							<label for="confSenha">Confirmar Senha:</label>
+							<input type="password" class="form-control" id="confSenha" name="confSenha" placeholder="Confirmar Senha"  >
 								</div>
 							</div>
 							
