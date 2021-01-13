cae66a4c939521214f802e03feff91ecfb0b9a1a
Aline Oliveira <alineoliveira199926@hotmail.com>
2021-01-12 23:13:30 -0300

    cadastro do login

--- View/Register.php
+++ View/Register.php
@@ -2,25 +2,48 @@
 //Página incial
 include '../ConexaoBanco.php';
 include '../ChamarBoostrap.php';
+include_once ('../config.php');
 
+
+if(isset($_REQUEST['submit']) and $_REQUEST['submit']!=""){
+	extract($_REQUEST);
+	if($privilegio==""){
+		$privilegio="paciente";
+		if($login==""){ //se o campo login estiver vazio...
+			header('location:'.$_SERVER['PHP_SELF'].'?msg=login'); //irá exibir isso no final da URL
+			exit;
+		}elseif($senha==""){
+			header('location:'.$_SERVER['PHP_SELF'].'?msg=senha');
+			exit;
+		}else{
+			
+			$userCount	=	$db->getQueryCount('login','idLogin');
+			$data	=	array(
+							'login'=>$login,
+							'senha'=>$senha,
+							'privilegio'=>$privilegio,
+						);
+			$insert	=	$db->insert('login',$data);
+			if($insert){
+				header('location:Register.php?msg=ras'); //adicionado com sucesso
+				exit;
+			}else{
+				header('location:Register.php?msg=rna'); //deu ruim, boy
+				exit;
+			}
+		}
+	}
+}
 
 ?>
 
 <!DOCTYPE html>
 <html>
 <head>
-
-
- 
-
-
   <title>Clinica médica</title>
 </head>
 <body>
 
-<head>
-
-<body>
 <?php
 	include 'nav.php';
 ?>
@@ -28,7 +51,35 @@ include '../ChamarBoostrap.php';
 	
 	
 		<div class="jumbotorn bg-primary" style=" margin-top:40px;  border-radius: 10px;">
-			
+		<?php
+
+		if(isset($_REQUEST['msg']) and $_REQUEST['msg']=="un"){
+
+			echo	'<div class="alert alert-danger"><i class="fa fa-exclamation-triangle"></i> User name is mandatory field!</div>';
+
+		}elseif(isset($_REQUEST['msg']) and $_REQUEST['msg']=="ue"){
+
+			echo	'<div class="alert alert-danger"><i class="fa fa-exclamation-triangle"></i> User email is mandatory field!</div>';
+
+		}elseif(isset($_REQUEST['msg']) and $_REQUEST['msg']=="up"){
+
+			echo	'<div class="alert alert-danger"><i class="fa fa-exclamation-triangle"></i> User phone is mandatory field!</div>';
+
+		}elseif(isset($_REQUEST['msg']) and $_REQUEST['msg']=="ras"){
+
+			echo	'<div class="alert alert-success"><i class="fa fa-thumbs-up"></i> Record added successfully!</div>';
+
+		}elseif(isset($_REQUEST['msg']) and $_REQUEST['msg']=="rna"){
+
+			echo	'<div class="alert alert-danger"><i class="fa fa-exclamation-triangle"></i> Record not added <strong>Please try again!</strong></div>';
+
+		}elseif(isset($_REQUEST['msg']) and $_REQUEST['msg']=="dsd"){
+
+			echo	'<div class="alert alert-danger"><i class="fa fa-exclamation-triangle"></i> Please delete a user and then try again <strong>We set limit for security reasons!</strong></div>';
+
+		}
+
+		?>
 
 		      <div class="text-center">
 		      	<h2 style="color: white; padding: 20px;"> Cadastro </h2>
@@ -39,7 +90,7 @@ include '../ChamarBoostrap.php';
 					<h3>Dados Pessoais:</h3>  
 					<hr>
 
-					<form>
+					<form method="post">
 					<div class="row form-group"  >
 							<div class="col-md-9 "  >
 							<div class="form-group">
@@ -52,7 +103,7 @@ include '../ChamarBoostrap.php';
 								<div class="form-group">
 									<label for="tipoSangue">Tipo Sanguíneo:</label>
 									<select class="form-control" id="tipoSangue">
-										<option value="N/a" selected>Não sei</option>
+										<option value="Na" selected>Não sei</option>
 										<option value="A+">A+</option>
 										<option value="A-">A-</option>
 										<option value="B+">B+</option>
@@ -187,8 +238,8 @@ include '../ChamarBoostrap.php';
 					<div class="row form-group">
 							<div class="col-md-6">
 							<div class="form-group">
-							<label for="Email">Email:</label>
-							<input type="email" class="form-control" id="Email"  placeholder="Email" name="Email">
+							<label for="login">Email:</label>
+							<input type="email" class="form-control" id="login"  placeholder="Email" name="login">
 								</div>
 							</div>
 
@@ -202,8 +253,8 @@ include '../ChamarBoostrap.php';
 
 							<div class="col-md-6">
 							<div class="form-group">
-							<label for="Senha">Senha:</label>
-							<input type="password" class="form-control" id="Senha"  placeholder="Senha" name="Senha" >
+							<label for="senha">Senha:</label>
+							<input type="password" class="form-control" id="senha"  placeholder="Senha" name="senha" >
 								</div>
 							</div>
 
@@ -219,7 +270,7 @@ include '../ChamarBoostrap.php';
 							</div>
 							<hr>
 							<div class="text-center">
-							<button type="button" class="btn btn-primary btn-lg btn-block"> Enviar cadastro </button>
+							<button type="submit" name="submit" value="submit" id="submit" class="btn btn-primary btn-lg btn-block"> Enviar cadastro </button>
 							</div>
 					</form>
 					
@@ -245,9 +296,6 @@ include '../ChamarBoostrap.php';
 
 	
 	
-
-
-</body>
 
 
 </body>
