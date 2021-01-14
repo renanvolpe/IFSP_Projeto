b66ea1febf0883fe73397685df4076b92296b3b8
Volpe <renan.volpe@aluno.ifsp.edu.br>
2021-01-14 02:15:24 -0300

    Arrumando o Banco geral
    Co-Authored-By: Salgreis <55090998+Salgreis@users.noreply.github.com>

--- View/calendario.php
+++ View/calendario.php
@@ -1,8 +1,9 @@
 <?php
 session_start();
 
-include 'NavMedico.php';
 
+
+include '../conexao.php';
 ?>
 
 <!DOCTYPE html>
--- conexao.php
+++ conexao.php
@@ -3,7 +3,7 @@
 define('HOST', 'localhost');
 define('USER', 'root');
 define('PASS', '');
-define('DBNAME', 'calendario');
+define('DBNAME', 'clinicamedica');
 define('PORT', 3306);
 
 $conn = new PDO('mysql:host=' . HOST . ';port='.PORT.';dbname=' . DBNAME . ';', USER, PASS);
