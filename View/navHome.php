8475f6ca0b26d726e91ab703171b161e509eff42
EeveeMarciano <45499911+EeveeMarciano@users.noreply.github.com>
2021-01-20 16:08:42 -0300

    terminado home
    mudado os incones e imagens,Tchau Bakugou

--- View/navHome.php
+++ View/navHome.php
@@ -19,6 +19,7 @@
                         <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#SobMedico">Sobre Medico</a></li>
                         <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#SobEspec">Sobre Especialidade</a></li>
 						<li class="nav-item"><a class="nav-link js-scroll-trigger" href="#sobreClin">Sobre a Clinica</a></li>
+						<li class="nav-item"><a class="nav-link js-scroll-trigger" href="#vantagens">Vantagens</a></li>
                         <li class="nav-item"><a class="nav-link js-scroll-trigger" href="#Local">Localidade</a></li>
                     </ul>
                 </div>
@@ -27,75 +28,3 @@
 
 </body>
 </html>
-<!--
-	<nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top py-3" id="mainNav">
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary border rounded p-2 mb-2">
-
-  <div class="container-fluid">
-	    <a class="navbar-brand" href="index.php">Home</a>
-	    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
-	      <span class="navbar-toggler-icon"></span>
-	    </button>
-	    <div class="collapse navbar-collapse" id="navbarNav">
-	      <ul class="navbar-nav">
-	        <li class="nav-item">
-	          <a class="nav-link active" aria-current="page" href="#apresentacao">Apresentacao</a>
-	        </li>
-	        <li class="nav-item">
-	          <a class="nav-link" href="#sintomas">Sintomas</a>
-	        </li>
-			<li class="nav-item">
-	          <a class="nav-link" href="#sabia">Você Sabia?</a>
-	        </li>
-			<li class="nav-item">
-	          <a class="nav-link" href="#especialidades">Especialidades</a>
-	        </li>
-			<li class="nav-item">
-	          <a class="nav-link" href="#tratamento">Tratamentos</a>
-	        </li>
-            <li class="nav-item">
-	          <a class="nav-link" href="#contatos">Contatos</a>
-	        </li>
-     	 </ul>
-		  <ul class="navbar-nav ml-auto">
-
-		  <li class="nav-item ml-auto">
-				<a href="View/Login.php" class="btn btn-outline-info navbar-brand bg-dark" > SUA CONSULTA AQUI </a>
-		  </li>
-		  </ul>
-
-	    </div>
-  </div>
-</nav>
-
-<nav class="navbar fixed-top navbar-expand-lg navbar-dark bg-primary border rounded p-2 mb-2">
-  <a class="navbar-brand" href="index.php">Home</a>
-  <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Alterna navegação">
-    <span class="navbar-toggler-icon"></span>
-  </button>
-  <div class="collapse navbar-collapse" id="navbarNav">
-    <ul class="navbar-nav">
-      <li class="nav-item active">
-        <a class="nav-link" href="#apresentacao">Apresentação</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link" href="#sabia">Você Sabia?</a>
-      </li>
-      <li class="nav-item">
-        <a class="nav-link disabled" href="#especialidades">Especialidades</a>
-      </li>
-	  <li class="nav-item">
-        <a class="nav-link disabled" href="#tratamento">Tratamentos</a>
-      </li>
-	  <li class="nav-item">
-        <a class="nav-link disabled" href="#contatos">Contatos</a>
-      </li>
-    </ul>
-	<ul class="navbar-nav ml-auto">
-
-		  <li class="nav-item ml-auto">
-				<a href="View/Login.php" class="btn btn-outline-info navbar-brand bg-dark" > SUA CONSULTA AQUI </a>
-		  </li>
-	 </ul>
-  </div>
-</nav>-->
--- index.php
+++ index.php
@@ -29,7 +29,7 @@ include 'ChamarBoostrap.php';
             </div>
 </header>
 <!--Agendar-->
-<section class="page-section bg-primary" id="about">
+<section class="page-section bg-primary" id="agende">
             <div class="container">
                 <div class="row justify-content-center">
                     <div class="col-lg-8 text-center">
@@ -80,11 +80,12 @@ include 'ChamarBoostrap.php';
             <div class="container">
                 <!-- Featured Row-->
                 <div class="row align-items-center bg-primary no-gutters mb-4 mb-lg-5">
-                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="Images/medicoBakugou.jpg" alt="" /></div>
+                    <div class="col-xl-8 col-lg-7"><img class="img-fluid mb-3 mb-lg-0" src="Images/consultoriosIndex.jpg" alt="" /></div>
                     <div class="col-xl-4 col-lg-5">
                         <div class="featured-text text-center text-lg-left">
-                            <h4 class="text-white mb-4">Shoreline</h4>
-                            <p class="text-white mb-4">Grayscale is open source and MIT licensed. This means you can use it for any project - even commercial projects! Download it, customize it, and publish your website!</p>
+                            <h4 class="text-white mb-4">Sobre Consutorio</h4>
+                            <p class="text-white mb-4">lacus fermentum eu. Interdum et malesuada f.sed nibh fermentum, tincidunt nulla eu, commodo quam. Quisque at hendrerit diam Interdum et malesuada fames ac ante ipsum primis in faucibus. Nulla augue ligula, euismod eu massa a, convallis elementum neque. Vivamus eget tristique nisl. Etiam vel neque scelerisque, suscipit nisi at, posuere nibh. Sed dignissim sem et sem convallis ultricies.
+                Integer mauris tortor, egestas ut dictum ut, dapibus eu dolor. Integer in luctus massa</p>
                         </div>
                     </div>
                 </div>
@@ -105,7 +106,7 @@ include 'ChamarBoostrap.php';
                 </div>
                 <!--  Two Row-->
                 <div class="row justify-content-center bg-primary no-gutters">
-                    <div class="col-lg-6"><img class="img-fluid" src="Images/preMar.jpg" alt="" /></div>
+                    <div class="col-lg-6"><img class="img-fluid" src="Images/predio.png" alt="" /></div>
                     <div class="col-lg-6 order-lg-first">
                         <div class="bg-black text-center h-100 project">
                             <div class="d-flex h-100">
@@ -123,34 +124,34 @@ include 'ChamarBoostrap.php';
 
 <section class="vant" id="vantagens">
             <div class="container">
-                <h2 class="text-center mt-0">At Your Service</h2>
-                <hr class="divider my-4" />
+                <h2 class="text-center mt-0">Vantagens</h2>
+                <hr class="divider text-info" />
                 <div class="row">
                     <div class="col-lg-3 col-md-6 text-center">
                         <div class="mt-5">
-                            <i class="fas fa-4x fa-gem text-primary mb-4"></i>
-                            <h3 class="text-white mb-4">Vantagens</h3>
+                            <i class="fas fa-4x fa-gem text-info mb-4"></i>
+                            <h3 class="text-white mb-4">Vantagens 1</h3>
                             <p class="text-white mb-4">Curabitur sit amet odio lobortis, luctus metus in, rhoncus nisl. Fusce et ligula vitae mi aliquet porttitor. Pellentesque convallis fringilla dui ut tincidunt. Sed consequat rutrum vulputate. Ut finibus vel dolor vitae ultrices. Sed tempor orci nulla, a ornare diam placerat iaculis. Nunc ac luctus turpis. Maecenas volutpat neque vel sem vehicula, et maximus sem rutrum. Fusce molestie vel lacus eget maximus. Fusce finibus, nisl ut malesuada tempus, erat enim venenatis risus, non lacinia tellus diam sed sem. Curabitur maximus velit est, at consectetur magna sagittis non.</p>
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6 text-center">
                         <div class="mt-5">
-                            <i class="fas fa-4x fa-laptop-code text-primary mb-4"></i>
-                            <h3 class="text-white mb-4">Vestibulum imperdiet nisi</h3>
+                            <i class="fas fa-4x fa-laptop-code text-info mb-4"></i>
+                            <h3 class="text-white mb-4">Vantagens 2</h3>
                             <p class="text-white mb-4">Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vulputate augue at lacus mattis, et viverra leo rutrum. Nullam et quam urna. Pellentesque vulputate lorem accumsan, mollis velit vitae, dictum augue. Maecenas quis neque tellus. Mauris ullamcorper ligula eu eros pulvinar auctor. Mauris lobortis augue ut tempor lacinia. Phasellus lobortis eleifend placerat. Fusce gravida lectus non dolor tincidunt tincidunt. Nulla sed sodales tellus, vitae efficitur enim. Duis ullamcorper sagittis risus a pulvinar. Donec efficitur ipsum eros, in tempus quam dapibus nec. Proin vel placerat quam, porttitor venenatis urna.</p>
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6 text-center">
                         <div class="mt-5">
-                            <i class="fas fa-4x fa-globe text-primary mb-4"></i>
-                            <h3 class="text-white mb-4">Lorem ipsum dolor sit amet.</h3>
+                            <i class="fas fa-4x fa-globe text-info mb-4"></i>
+                            <h3 class="text-white mb-4">Vantagens 3</h3>
                             <p class="text-white mb-4">Phasellus luctus id sapien et vestibulum. Morbi vitae quam nec justo lobortis iaculis et a nunc. Aliquam luctus gravida orci vitae pretium. Integer eros risus, commodo a ex non, bibendum ultricies felis. Suspendisse pulvinar quam id mauris luctus euismod. Sed pharetra, justo id interdum vehicula, magna ligula pretium diam, a volutpat augue ante fringilla nulla. Duis ac tortor suscipit, molestie sapien a, feugiat nulla. Lorem ipsum dolor sit amet, consectetur adipiscing elit. Pellentesque id sagittis lacus. Sed quis posuere nunc.</p>
                         </div>
                     </div>
                     <div class="col-lg-3 col-md-6 text-center">
                         <div class="mt-5">
-                            <i class="fas fa-4x fa-heart text-primary mb-4"></i>
-                            <h3 class="text-white mb-4">Lorem ipsum dolor sit amet.</h3>
+                            <i class="fas fa-4x fa-heart text-info mb-4"></i>
+                            <h3 class="text-white mb-4">Vantagens 4</h3>
                             <p class="text-white mb-4">Morbi molestie ullamcorper sodales. Donec velit mi, gravida quis dolor a, sodales ornare magna. Nam sit amet ligula at dui facilisis luctus a vel elit. Phasellus in erat in erat semper iaculis. Sed ut enim ut dolor finibus pharetra. Cras ut vulputate ante, non fermentum leo. Sed ac consequat nisl, eget ornare mauris. Sed mattis suscipit ullamcorper. Vestibulum feugiat, metus a ullamcorper placerat, ligula orci lobortis mauris, sit amet volutpat dui ante suscipit eros. Maecenas vel enim ex. In ex nulla, tincidunt eu sem sit amet, fermentum faucibus tellus. Integer quis nulla turpis.</p>
                         </div>
                     </div>
