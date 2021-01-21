975e85f318db5b70b79e756f40ad62ee24ffc379
Aline Oliveira <alineoliveira199926@hotmail.com>
2021-01-20 23:26:26 -0300

    edit mas não funciona

--- controller/edit_event.php
+++ controller/edit_event.php
@@ -12,7 +12,10 @@ $data_start_conv = date("Y-m-d H:i:s", strtotime($data_start));
 $data_end = str_replace('/', '-', $dados['end']);
 $data_end_conv = date("Y-m-d H:i:s", strtotime($data_end));
 
-$query_event = "UPDATE agenda SET title=:title, color=:color, start=:start, end=:end WHERE idAgenda=:idAgenda";
+$data_paciente_conv=(int)$dados['paciente_idPaciente'];
+$data_medico_conv=(int)$dados['medico_idMedico'];
+
+$query_event = "UPDATE agenda SET title=:title, color=:color, start=:start, end=:end, paciente_idPaciente=:paciente_idPaciente, medico_idMedico=:medico_idMedico WHERE idAgenda=:idAgenda";
 
 $update_event = $conn->prepare($query_event);
 $update_event->bindParam(':title', $dados['title']);
@@ -20,6 +23,8 @@ $update_event->bindParam(':color', $dados['color']);
 $update_event->bindParam(':start', $data_start_conv);
 $update_event->bindParam(':end', $data_end_conv);
 $update_event->bindParam(':idAgenda', $dados['idAgenda']);
+$update_event->bindParam(':paciente_idPaciente', $data_paciente_conv);
+$update_event->bindParam(':medico_idMedico',$data_medico_conv );
 
 if ($update_event->execute()) {
     $retorna = ['sit' => true, 'msg' => '<div class="alert alert-success" role="alert">Evento editado com sucesso!</div>'];
